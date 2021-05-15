2123b056a6d18e6ca36a919b5a466dd6d2324ca5
Ruslan Gonzalez <ruslanguns@gmail.com>
2021-05-15 20:02:44 +0100

    New endpoint '/random' returns random user

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from './core/prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly data: PrismaService) {}
+
+  /**
+   * This method returns a User randomly
+   */
+  async getRandomUser() {
+    const count = await this.data.user.count();
+    const randomNumber = Math.floor(Math.random() * count) + 1;
+
+    if (!count) {
+      throw new NotFoundException('Still no available data, please seeed it');
+    }
+
+    return await this.data.user.findFirst({ take: 1, skip: randomNumber });
   }
 }
---
+++ src/core/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '.prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super();
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
